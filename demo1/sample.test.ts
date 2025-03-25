5227ac18c3f7c2e916417c16580743a8020ffde9
Oscar Javier <oscarjavierdos@gmail.com>
2025-03-25 09:23:26 +0100

    Refactor add tests for clarity and readability

--- demo1/sample.test.ts
+++ demo1/sample.test.ts
@@ -9,7 +9,11 @@ import { add } from './sample';
 
 describe('add tests', () => {
     test('it should be 5 when add 2 and 3', () => {
-        expect(add(2, 3)).toBe(5);
+        const num1 = 2;
+        const num2 = 3;
+        const expectd = 5;
+        const result = add(num1, num2);
+        expect(result).toBe(expectd);
     });
     test('it should be -5 when add -2 and -3', () => {
         expect(add(-2, -3)).toBe(-5);
